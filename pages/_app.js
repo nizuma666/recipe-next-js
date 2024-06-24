b3b9e863eae2a19ce38fb9fe81516150e3bf49a6
syaifullohismail <syaifullohismail123@gmail.com>
2024-06-24 20:09:58 +0700

    change import directory layout in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 // import { Provider } from "react-redux";
-import Layout from "../components/Layout";
+import Layout from "/components/layout";
 import "../styles/globals.css";
 import { wrapper } from "../configs/store";
 
 function App({ Component, pageProps }) {
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
-export default wrapper.withRedux(App)
+export default wrapper.withRedux(App);
